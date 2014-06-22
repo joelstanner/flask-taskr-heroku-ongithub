074d85b951d89b4eaf3d78a0522f9fbdcba423da
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-06-22 13:20:05 -0700

    fabs

--- fabfile.py
+++ fabfile.py
@@ -6,11 +6,11 @@ def test():
     local("python test_tasks.py -v && python test_users.py -v")
     
 def commit():
-    message = raw_input("Enter a git command message:  ")
+    message = raw_input("Enter a git commit message:  ")
     local("git add . && git commit -am '{}'".format(message))
     
 def push():
-    local("git push origin master")
+    local("git push heroku master")
     
 def prepare():
     test()
